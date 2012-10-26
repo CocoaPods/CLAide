a7708870ccc4f9e1920d197c8118b7dc31ec96ef
Eloy Durán <eloy.de.enige@gmail.com>
2012-10-26 21:13:44 +0200

    Update argv.rb example

--- examples/argv.rb
+++ examples/argv.rb
@@ -5,10 +5,21 @@ $:.unshift(File.expand_path('../../lib', __FILE__))
 require 'claide'
 
 argv = CLAide::ARGV.new(['tea', '--no-milk', '--sweetner=honey'])
-p argv.arguments          # => ['tea']
 p argv.shift_argument     # => 'tea'
+p argv.shift_argument     # => nil
 p argv.flag?('milk')      # => false
+p argv.flag?('milk')      # => nil
 p argv.option('sweetner') # => 'honey'
+p argv.option('sweetner') # => nil
+
+puts
+
+argv = CLAide::ARGV.new(['tea', 'coffee'])
+p argv.arguments  # => ['tea', 'coffee']
+p argv.arguments! # => ['tea', 'coffee']
+p argv.arguments  # => []
+
+puts
 
 argv = CLAide::ARGV.new(['tea'])
 p argv.flag?('milk', true)         # => true
