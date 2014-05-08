a692c3afb6324d25266cb4a984a93a9af5e3629e
Fabio Pelosin <fabiopelosin@gmail.com>
2014-05-08 10:13:30 +0200

    [Specs] Generate coverage only on Ruby > 1.8.7 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,14 @@
 if RUBY_VERSION >= '1.9.3'
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
-
-  require 'pathname'
-  ROOT = Pathname.new(File.expand_path('../../', __FILE__))
-  $:.unshift((ROOT + 'lib').to_s)
-  $:.unshift((ROOT + 'spec').to_s)
 end
 
 #-----------------------------------------------------------------------------#
+
+require 'pathname'
+ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+$:.unshift((ROOT + 'lib').to_s)
+$:.unshift((ROOT + 'spec').to_s)
 
 require 'bundler/setup'
 require 'bacon'
