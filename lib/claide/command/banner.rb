8152a5cb5cbbbb2193856b973bf220972419cc28
Fabio Pelosin <fabiopelosin@gmail.com>
2014-05-09 22:34:23 +0200

    [Banner] Refactor to reduce cyclomatic complexity

--- lib/claide/command/banner.rb
+++ lib/claide/command/banner.rb
@@ -45,55 +45,68 @@ module CLAide
       #
       def formatted_usage_description
         if message = command.description || command.summary
-          message = strip_heredoc(message)
-          message = message.split("\n").map { |line| "      #{line}" }.join("\n")
-          args = " #{command.arguments}" if command.arguments
-          command_signature = prettify_command_in_usage_description(command.full_command, args)
-          "    $ #{command_signature}\n\n#{message}"
+          message_lines = strip_heredoc(message).split("\n")
+          message_lines = message_lines.map { |l| l.insert(0, ' ' * 6) }
+          formatted_message = message_lines.join("\n")
+
+          signature = prettify_signature(command)
+          "$ #{signature}\n\n#{formatted_message}".insert(0, ' ' * 4)
         end
       end
+
+      # @return [String] The indentation of the subcommands and of the options
+      #         names.
+      #
+      NAME_INDENTATION = 4
+
+      # @return [String] The minimum between a name and its description.
+      #
+      DESCRIPTION_SPACES = 3
 
       # @return [String] The section describing the subcommands of the command.
       #
       def formatted_subcommand_summaries
-        subcommands = command.subcommands_for_command_lookup.reject do |subcommand|
-          subcommand.summary.nil?
-        end.sort_by(&:command)
+        subcommands = subcommands_for_banner
         unless subcommands.empty?
           command_size = subcommands.map { |cmd| cmd.command.size }.max
           subcommands.map do |subcommand|
-            subcommand_string = subcommand.command.ljust(command_size)
-            subcommand_string = prettify_subcommand_name(subcommand_string)
-            is_default = subcommand.command == command.default_subcommand
-            if is_default
-              bullet_point = '>'
-            else
-              bullet_point = '*'
-            end
-            "    #{bullet_point} #{subcommand_string}   #{subcommand.summary}"
+            subcommand_summary(subcommand, command_size)
           end.join("\n")
         end
+      end
+
+      # @return [String] The line describing a single subcommand.
+      #
+      def subcommand_summary(subcommand, command_size)
+        subcommand_string = subcommand.command.ljust(command_size)
+        subcommand_string = prettify_subcommand(subcommand_string)
+        is_default = subcommand.command == command.default_subcommand
+        if is_default
+          bullet_point = '>'
+        else
+          bullet_point = '*'
+        end
+        "    #{bullet_point} #{subcommand_string}   #{subcommand.summary}"
       end
 
       # @return [String] The section describing the options of the command.
       #
       def formatted_options_description
-        opts = command.options
-        max_key_size = opts.map { |opt| opt.first.size }.max
-
-        desc_start = max_key_size + 7 # fixed whitespace in `result` var
-        desc_width = terminal_width - desc_start
-
-        opts.map do |key, desc|
-          space = ' ' * (max_key_size - key.size)
-          result = "    #{prettify_option_name(key)}#{space}   "
-          if terminal_width == 0
-            result << desc
-          else
-            space = ' ' * desc_start
-            result << word_wrap(desc, desc_width).split("\n").join("\n#{space}")
-          end
+        options = command.options
+        max_key_size = options.map { |option| option.first.size }.max
+        options.map do |name, description|
+          option_description(name, description, max_key_size)
         end.join("\n")
+      end
+
+      # @return [String] The line describing a single option.
+      #
+      def option_description(name, description, max_name_width)
+        result = prettify_option_name(name).ljust(max_name_width)
+        result.insert(0, ' ' * NAME_INDENTATION)
+        result.insert(-1, ' ' * DESCRIPTION_SPACES)
+        desc_start = max_name_width + NAME_INDENTATION + DESCRIPTION_SPACES
+        result << wrap_with_indent(description, desc_start)
       end
 
       # @!group Subclasses overrides
@@ -108,21 +121,29 @@ module CLAide
 
       # @return [String] A decorated textual representation of the command.
       #
-      def prettify_command_in_usage_description(command, args)
-        result = "#{command.ansi.green}"
-        result << "#{args.ansi.magenta}" if args
+      def prettify_signature(command)
+        result = "#{command.full_command.ansi.green}"
+        result << " #{command.arguments.ansi.magenta}" if command.arguments
         result
       end
 
       # @return [String] A decorated textual representation of the subcommand
       #         name.
       #
-      def prettify_subcommand_name(name)
+      def prettify_subcommand(name)
         name.ansi.green
       end
 
       # @!group Private helpers
       #-----------------------------------------------------------------------#
+
+      # @return [String]
+      #
+      def subcommands_for_banner
+        command.subcommands_for_command_lookup.reject do |subcommand|
+          subcommand.summary.nil?
+        end.sort_by(&:command)
+      end
 
       # @return [String] Lifted straight from ActiveSupport. Thanks guys!
       #
@@ -131,6 +152,25 @@ module CLAide
           string.gsub(/^[ \t]{#{min.size}}/, '')
         else
           string
+        end
+      end
+
+      # @return [String] Wraps a string to the terminal width taking into
+      #         account the given indentation.
+      #
+      # @param  [String] string
+      #         The string to indent.
+      #
+      # @param  [Fixnum] indent
+      #         The number of spaces to insert before the string.
+      #
+      def wrap_with_indent(string, indent)
+        if terminal_width == 0
+          string
+        else
+          width = terminal_width - indent
+          space = ' ' * indent
+          word_wrap(string, width).split("\n").join("\n#{space}")
         end
       end
 
